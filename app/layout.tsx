73eb07c96e4a508304e7376d45667c3b406e6636
rrh1441 <rrh1441@gmail.com>
2025-04-15 12:25:02 -0700

    minor updates v2

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'WP Offramp',
+  description: 'Leave WordPress Behind',
+  generator: 'v0.dev + gemini + claude + chatgpt',
 }
 
 export default function RootLayout({
